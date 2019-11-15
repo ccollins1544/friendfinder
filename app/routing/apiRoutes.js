545be95f1b00ebdf78f86ec73c7477eaa820ce88
Christopher Collins <chris@ccollins.io>
2019-11-14 18:08:38 -0700

    Added questions api route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,15 +3,21 @@
  * @package friendfinder
  *****************************************/
 // Load Data
-
 var friends = require("../data/friends");
+var questions = require("../data/questions");
 
 // Routing
 module.exports = function(app) {
+  // GET Routes
   app.get("/api/friends", function(req, res){
     res.json(friends);
   });
 
+  app.get("/api/questions", function(req, res){
+    res.json(questions);
+  });
+
+  // POST Routes
   app.post("/api/friends", function(req, res){
     friends.push(req.body);
     res.json(true);
